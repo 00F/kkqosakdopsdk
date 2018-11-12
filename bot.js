549abebd36a953f7223d218df963b2f6e46fe36e
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-12 15:42:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['463698464845594628'];
+const developers = ['463698464845594628','320423357709549568'];
 
 
 client.on('ready', () => {
@@ -15,21 +15,24 @@ client.on('message', message => {
 
 if (message.content.startsWith('Rwt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("dnd")
     message.channel.send(`${argresult} \` `)
 } else 
 if (message.content.startsWith('Rls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("dnd")
     message.channel.send(` ${argresult} \` `)
 } else 
 if (message.content.startsWith('Rst')) {
   client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
+  client.user.setStatus("dnd")
    message.channel.send(`${argresult} \` `)
 }
 });
 
 client.on('message', message => {
         if (!developers.includes(message.author.id)) return;
-  if (message.content === '712') {
+  if (message.content === '..1') {
   let channel = client.channels.get('5114568742819594250');
 
   channel.join()
